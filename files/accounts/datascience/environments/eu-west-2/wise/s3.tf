417050417f6626a8ed21a294d7028c88769729ca
Linh Nguyen <linh5847@gmail.com>
2026-02-06 10:35:11 +0000

    trial1

--- files/accounts/datascience/environments/eu-west-2/wise/s3.tf
+++ files/accounts/datascience/environments/eu-west-2/wise/s3.tf
@@ -1,12 +1,12 @@
 module "s3_bucket" {
-  depends_on = [module.vp, module.internet-gateway]
-  source     = "git@github.com/vntechsol/aws-iac.git//modules/s3/bucket"
+  # tflint-ignore: terraform_module_pinned_source
+  source = "git@github.com:vntechsol/aws-iac.git//modules/s3/bucket"
 
-  enabled        = true
-  s3_bucket_name = "wise_s3_bucket"
+  enabled        = "true"
+  s3_bucket_name = "infratests3bucket"
   additional_tags = {
-    "Cost Centre" = "IaC"
-    Product       = "IaC"
-    Service       = "Infra"
+    "Cost Centre" = "INFRA"
+    Product       = "INFRA"
+    Service       = "IaC"
   }
 }
