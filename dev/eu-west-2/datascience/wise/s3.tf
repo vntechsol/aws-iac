c8d8c3046506fbfec93d884cb29db6c0058fdd31
Linh Nguyen <linh5847@gmail.com>
2026-02-06 13:54:32 +0000

    trial2

--- dev/eu-west-2/datascience/wise/s3.tf
+++ dev/eu-west-2/datascience/wise/s3.tf
@@ -1,7 +1,7 @@
 module "s3_bucket" {
   #checkov:skip=CKV_TF_2 Ignore module tag
   # tflint-ignore: terraform_module_pinned_source
-  source = "../../../../../../modules/s3/bucket" #"git@github.com:vntechsol/aws-iac.git//modules/s3/bucket"
+  source = "../../../../modules/s3/bucket" #"git@github.com:vntechsol/aws-iac.git//modules/s3/bucket"
 
   enabled        = "true"
   s3_bucket_name = "infratests3bucket"
