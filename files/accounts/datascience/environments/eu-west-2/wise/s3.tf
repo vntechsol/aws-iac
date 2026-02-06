2dce5816b1977ba7818937495309c1e98eedeaab
Linh Nguyen <linh5847@gmail.com>
2026-02-06 22:26:17 +0000

    TRial1

---
+++ files/accounts/datascience/environments/eu-west-2/wise/s3.tf
@@ -0,0 +1,9 @@
+module "s3_bucket" {
+  #checkov:skip=CKV_TF_2 Ignore module tag
+  # tflint-ignore: terraform_module_pinned_source
+  source = "../../../../../../modules/s3/bucket" #"git@github.com:vntechsol/aws-iac.git//modules/s3/bucket"
+
+  enabled         = var.config.s3_bucket.enabled
+  s3_bucket_name  = var.config.s3_bucket.s3_bucket_name
+  additional_tags = var.additional_tags
+}
